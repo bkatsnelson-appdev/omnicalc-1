86d5097e525a7b99eec58c4a35d27d82057e1c18
beatricek222 <beatricek222@gmail.com>
2022-10-14 03:56:57 +0000

    Square form working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   def blank_square_form
-    render({ :template => "calculation_templates/square_form.html.erb"})
+    render({ :template => "calculation_templates/square_form.html.erb" })
   end
 
+  def calculate_square
+    #params = {"number" => "42"}
+
+    @num = params.fetch("number").to_f
+    @square_of_num = @num * @num
+    render({ :template => "calculation_templates/square_results.html.erb" })
+  end
 end
